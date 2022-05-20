6758dd34bb582990c132f86fab50a881361ad870
Ameer Hamza <ameer.hamza@itb.local>
2022-05-20 12:16:12 +0500

    Notification send

--- App.js
+++ App.js
@@ -1,20 +1,47 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, View, Button } from "react-native"
+import * as Notifications from "expo-notifications"
+
+
+Notifications.setNotificationHandler({
+  handleNotification: async () => {
+    return {
+      shouldShowAlert: true,
+    }
+  },
+})
 
 export default function App() {
+
+
+  const triggerLocalNotificationHandler = () => {
+    Notifications.scheduleNotificationAsync({
+      content: {
+        title: "Hello World!",
+        body: "Hi Hamza",
+      },
+      trigger: { seconds: 5 },
+    })
+  }
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Button
+        title="Trigger Local Notification"
+        onPress={triggerLocalNotificationHandler}
+      />
     </View>
-  );
+  )
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
-});
+})
+
+
+
+
